ba6343f8eac4155556e908e2ddd20bf6d81c876e
Jayadeep KM <kmjayadeep@gmail.com>
2015-10-17 08:00:27 +0530

    initial commit

---
+++ src/irail/GlobalConstants.java
@@ -0,0 +1,10 @@
+package irail;
+
+/**
+ *
+ * @author jayadeep
+ */
+public class GlobalConstants {
+    public static String connectionUrl="jdbc:mysql://localhost:3306/railway?user=root&password=";
+    public static String registerDriver="com.mysql.jdbc.Driver";
+}
---
+++ src/irail/MainController.java
@@ -0,0 +1,13 @@
+package irail;
+
+/**
+ *
+ * @author jayadeep
+ */
+public class MainController {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
